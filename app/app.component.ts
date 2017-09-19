c0189296b997abdca87fffaad796723c92e9c369
Evan Johnson and Haneen Abukhater <evan@>
2017-09-19 16:50:28 -0700

    update components

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,20 +6,23 @@ import {Component} from '@angular/core';
   <div class="container">
     <h1>Recipe App</h1>
 
-    <ul>
-      <li *ngFor= "let recipe of allRecipes">{{recipe.title}}
-      <p> <strong> Ingredients: </strong> </p>
-        <ol>
-          <li *ngFor= "let hummus of recipe.ingredients">{{hummus}}</li>
-        </ol>
-        <hr>
-        <p><strong> Directions: </strong></p>
-        <ol>
-          <li *ngFor="let haneen of recipe.directions">{{haneen}}</li>
-        </ol>
-      </li>
-      <br>
-    </ul>
+
+    <div *ngFor= "let recipe of allRecipes">
+      <ul>
+        <h1 class='jumbotron'>{{recipe.title}}</h1>
+          <p> <strong> Ingredients: </strong> </p>
+            <ol>
+              <li [class]="goodness(recipe)" *ngFor= "let hummus of recipe.ingredients">{{hummus}}</li>
+            </ol>
+            <hr>
+            <p><strong> Directions: </strong></p>
+            <ol>
+              <li (click)='isItGood(recipe)' *ngFor="let haneen of recipe.directions">{{haneen}}</li>
+            </ol>
+          <br>
+          <button (click)="editTask()">Edit!</button>
+        </ul>
+      </div>
   </div>
   `
 })
@@ -27,13 +30,35 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   allRecipes: Recipe [] =[
-    new Recipe('Cookies', ['flour', 'butter', 'sugar'], ['mix all ingredients together', 'bake the cookies']),
-    new Recipe('Hummus', ['chickpea', 'tahini', 'olive oil', 'garlic', 'lemon juice'], ['empty into container', 'blend it up', 'hide behind a curtain','devour']),
-    new Recipe('Bagels', ['flour', 'sugar', 'patience'], ['kneed dough together', 'let sit in fridge over night', 'boil for 60 seconds', 'toss em in the oven', 'cream cheese and enjoy!'])
+    new Recipe('Cookies', ['flour', 'butter', 'sugar'], ['mix all ingredients together', 'bake the cookies'], 9),
+    new Recipe('Hummus', ['chickpea', 'tahini', 'olive oil', 'garlic', 'lemon juice'], ['empty into container', 'blend it up', 'hide behind a curtain','devour'], 12),
+    new Recipe('Bagels', ['flour', 'sugar', 'patience'], ['kneed dough together', 'let sit in fridge over night', 'boil for 60 seconds', 'toss em in the oven', 'cream cheese and enjoy!'], 7)
   ]
+  
+  goodness(recipe: Recipe) {
+    if (recipe.goodness < 9) {
+      return 'bg-danger';
+    } else if (recipe.goodness < 10) {
+      return 'bg-warning';
+    } else {
+      return 'bg-info';
+    }
+  }
+
+  editTask(){
+    alert("You just requested to edit the Recipe");
+  }
+
+  isItGood(entireRecipeObject: Recipe) {
+    alert(entireRecipeObject.yummy);
+  }
+  payAttention() {
+    alert('You should be cooking!');
+  }
 }
 
 export class Recipe{
-  constructor(public title: string, public ingredients: string[], public directions: string []){}
+  public yummy: boolean = true;
+  constructor(public title: string, public ingredients: string[], public directions: string [], public goodness: number){}
 
 }
